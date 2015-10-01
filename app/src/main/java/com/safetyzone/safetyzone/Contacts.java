4e193d99188b465da06f06d7e38cd196386945b8
Mitchell Busby <mitchbusby@gmail.com>
2015-10-01 18:13:04 +1000

    Basic list view implemented, still very much static

--- app/src/main/java/com/safetyzone/safetyzone/Contacts.java
+++ app/src/main/java/com/safetyzone/safetyzone/Contacts.java
@@ -4,7 +4,15 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
 import android.widget.Toolbar;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class Contacts extends AppCompatActivity {
     @Override
@@ -12,6 +20,9 @@ public class Contacts extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_contacts);
         onCreateCustom();
+        List<String> values = Arrays.asList("Jess D'Ali", "Mitchell Busby", "Bec Lyons");
+        populateContactList(values);
+
     }
     public void onCreateCustom() {
         final android.support.v7.widget.Toolbar toolbar = (android.support.v7.widget.Toolbar) findViewById(R.id.mainToolbar);
@@ -21,7 +32,28 @@ public class Contacts extends AppCompatActivity {
         } catch (NullPointerException e) {
 
         }
-        toolbar.setTitle("Blah");
+        //toolbar.setTitle("Blah");
+    }
+    public void addContact(ContactsAdapter<String> contactsAdapter) {
+        contactsAdapter.add("New Contact");
+    }
+    public void addListenerToListModifierButton(final ContactsAdapter<String> contactsAdapter) {
+        final Button button = (Button) findViewById(R.id.add_to_list_button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                addContact(contactsAdapter);
+            }
+        });
+    }
+    public void populateContactList(List<String> values) {
+        final ListView listView = (ListView) findViewById(R.id.contacts_list);
+        /*String[] values = new String[] {
+                "Blah"
+        };*/
+        ContactsAdapter<String> arrayAdapter = new ContactsAdapter(getBaseContext(), R.layout.list_row, R.id.list_contact_name,values);
+        listView.setAdapter(arrayAdapter);
+        addListenerToListModifierButton(arrayAdapter);
     }
     /*
     @Override
