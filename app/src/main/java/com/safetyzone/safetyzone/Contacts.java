0226f0dbcfcc1f69b7888b51c015932b40fccd68
Mitchell Busby <mitchbusby@gmail.com>
2015-10-01 15:17:24 +1000

    Initial commit with app compatibility down to ICS, also has material design stuff.

--- app/src/main/java/com/safetyzone/safetyzone/Contacts.java
+++ app/src/main/java/com/safetyzone/safetyzone/Contacts.java
@@ -1,22 +1,33 @@
 package com.safetyzone.safetyzone;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toolbar;
 
 public class Contacts extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_contacts);
+        onCreateCustom();
     }
+    public void onCreateCustom() {
+        final android.support.v7.widget.Toolbar toolbar = (android.support.v7.widget.Toolbar) findViewById(R.id.mainToolbar);
+        setSupportActionBar(toolbar);
+        try {
+            getSupportActionBar().setTitle("Blah");
+        } catch (NullPointerException e) {
 
+        }
+        toolbar.setTitle("Blah");
+    }
+    /*
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_contacts, menu);
+        //getMenuInflater().inflate(R.menu.menu_contacts, menu);
         return true;
     }
 
@@ -33,5 +44,6 @@ public class Contacts extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
-    }
+
+    }*/
 }
