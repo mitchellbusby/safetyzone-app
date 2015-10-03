e4df7446e346b274a621bf86acd711267bbc78ce
Mitchell Busby <mitchbusby@gmail.com>
2015-10-03 15:23:37 +1000

    Adds stricter layout types to navigation

--- app/src/main/java/com/safetyzone/safetyzone/GeneralActivity.java
+++ app/src/main/java/com/safetyzone/safetyzone/GeneralActivity.java
@@ -3,24 +3,35 @@ package com.safetyzone.safetyzone;
 import android.app.FragmentTransaction;
 import android.content.Context;
 import android.os.Bundle;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.LayoutInflater;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ListAdapter;
 import android.widget.ListView;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class GeneralActivity extends AppCompatActivity {
+    android.support.v7.widget.Toolbar toolbar;
+    DrawerLayout mDrawerLayout;
+    ActionBarDrawerToggle mDrawerToggle;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_general);
         onCreateCustom();
         addContactsFragment();
+        initialiseNavigationLinks();
+        initialiseNavigationDrawer();
     }
     public void onCreateCustom() {
-        final android.support.v7.widget.Toolbar toolbar = (android.support.v7.widget.Toolbar) findViewById(R.id.mainToolbar);
+        toolbar = (android.support.v7.widget.Toolbar) findViewById(R.id.mainToolbar);
         setSupportActionBar(toolbar);
         try {
             getSupportActionBar().setTitle("Blah");
@@ -30,12 +41,54 @@ public class GeneralActivity extends AppCompatActivity {
 
         //toolbar.setTitle("Blah");
     }
+    public void initialiseNavigationDrawer() {
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.activity_contacts);
+        mDrawerToggle = new ActionBarDrawerToggle(this, mDrawerLayout, toolbar, R.string.app_name, R.string.app_name) {
+            /** Called when a drawer has settled in a completely closed state. */
+            public void onDrawerClosed(View view) {
+                super.onDrawerClosed(view);
+                //getSupportActionBar().setTitle("Closed");
+            }
+
+            /** Called when a drawer has settled in a completely open state. */
+            public void onDrawerOpened(View drawerView) {
+                super.onDrawerOpened(drawerView);
+                getSupportActionBar().setTitle("Open");
+            }
+        };
+        mDrawerLayout.setDrawerListener(mDrawerToggle);
+    }
+    @Override
+    protected void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        // Sync the toggle state after onRestoreInstanceState has occurred.
+        mDrawerToggle.syncState();
+    }
+    public void initialiseNavigationLinks() {
+        List<String> mDrawerList = new ArrayList<String>();
+        mDrawerList.add("Link 1");
+        ListView mDrawerListView = (ListView) findViewById(R.id.nav_list);
+        ListAdapter drawerListAdapter = new ArrayAdapter<String>(getBaseContext(), R.layout.list_navigation, R.id.list_navigation_item, mDrawerList);
+        mDrawerListView.setAdapter(drawerListAdapter);
+        //mDrawerListView.setOnItemClickListener(new DrawerItemClickListener());
+
+    }
     public void addContactsFragment() {
         android.support.v4.app.FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
         ft.replace(R.id.fragment_placeholder, new ContactsFragment());
         ft.commit();
     }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Pass the event to ActionBarDrawerToggle, if it returns
+        // true, then it has handled the app icon touch event
+        if (mDrawerToggle.onOptionsItemSelected(item)) {
+            return true;
+        }
+        // Handle your other action bar items...
 
+        return super.onOptionsItemSelected(item);
+    }
     /*
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
