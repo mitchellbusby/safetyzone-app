1a637c122981f0647072cf7ab9d0858154a6c506
Mitchell Busby <mitchbusby@gmail.com>
2015-10-31 18:43:15 +1100

    Adds more service data

--- app/src/main/java/com/safetyzone/safetyzone/SafeService.java
+++ app/src/main/java/com/safetyzone/safetyzone/SafeService.java
@@ -1,5 +1,15 @@
 package com.safetyzone.safetyzone;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLConnection;
 import java.util.Random;
 
 /**
@@ -10,6 +20,30 @@ public class SafeService {
         // Do nothing inside the constructor just yet
     }
     public int getSafetyRating(double longitude, double latitude) {
+        String result = getData(longitude, latitude);
+        JSONObject resultAsJson = null;
+        try { resultAsJson = new JSONObject(result); }
+        catch (JSONException e) { resultAsJson = null; }
         return new Random().nextInt(3);
+    }
+    public String getData(double longitude, double latitude) {
+        //return "";
+        try {
+            URL url = new URL("http://safetyzone.azurewebsites.net/api/CrimeStats?latitude={0}&longitude={1}".format(String.valueOf(latitude), String.valueOf(longitude)));
+            HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+            InputStream inputStream = new BufferedInputStream(urlConnection.getInputStream());
+            BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
+            String line = null;
+            StringBuilder sb = new StringBuilder();
+            while ((line = reader.readLine()) != null) {
+                sb.append(line+'\n');
+            }
+            String result = sb.toString();
+            return result;
+        }
+        catch (Exception e) {
+            System.out.println("Failed connecting to server");
+            return null;
+        }
     }
 }
