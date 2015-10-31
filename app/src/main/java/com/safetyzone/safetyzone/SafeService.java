c8f798c7023e66459e06a5405c2ddb4050a722fd
Mitchell Busby <mitchbusby@gmail.com>
2015-10-31 17:38:17 +1100

    Adds more safety rating data

--- app/src/main/java/com/safetyzone/safetyzone/SafeService.java
+++ app/src/main/java/com/safetyzone/safetyzone/SafeService.java
@@ -1,10 +1,15 @@
 package com.safetyzone.safetyzone;
 
+import java.util.Random;
+
 /**
  * Created by Zak on 31/10/2015.
  */
 public class SafeService {
     public SafeService() {
         // Do nothing inside the constructor just yet
+    }
+    public int getSafetyRating(double longitude, double latitude) {
+        return new Random().nextInt(3);
     }
 }
