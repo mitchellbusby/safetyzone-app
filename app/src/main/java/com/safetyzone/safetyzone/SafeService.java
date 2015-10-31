94ce871a3cc4813b733354502cb0568625a6f454
Mitchell Busby <mitchbusby@gmail.com>
2015-10-31 17:33:23 +1100

    Adds constructor

--- app/src/main/java/com/safetyzone/safetyzone/SafeService.java
+++ app/src/main/java/com/safetyzone/safetyzone/SafeService.java
@@ -4,5 +4,7 @@ package com.safetyzone.safetyzone;
  * Created by Zak on 31/10/2015.
  */
 public class SafeService {
-
+    public SafeService() {
+        // Do nothing inside the constructor just yet
+    }
 }
